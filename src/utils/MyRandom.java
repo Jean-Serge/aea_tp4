e29f636ca28e24cf4edd65d65eb661970084d471
Jean-Serge <j.monbailly@orange.Fr>
2015-03-13 11:48:18 +0100

    Commentaires.

--- src/utils/MyRandom.java
+++ src/utils/MyRandom.java
@@ -9,6 +9,12 @@ public class MyRandom extends Random {
 	 */
 	private static final long serialVersionUID = 4505783718180659211L;
 
+	/**
+	 * Retourne une valeur aléatoire positive en utilisant la fonction next de Random pour
+	 * définir le nombre de bit à utiliser.
+	 * 
+	 * @return une valeur aléatoire positive.
+	 */
 	public int nextPositive(){
 		return next(31);
 	}
