67d6fdd26496a9a54a6e651609f840c367bae82f
Jean-Serge <j.monbailly@orange.Fr>
2015-03-13 11:34:54 +0100

    Ajout d'une classe Main permettant de vérifier le comportement de la génération aléatoire du graphe.

---
+++ src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import entite.GrapheAleatoire;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		GrapheAleatoire g = new GrapheAleatoire(10, 8);
+		System.out.println(g);
+
+	}
+
+}
