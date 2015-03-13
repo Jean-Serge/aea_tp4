09533dc86d8e60c53e66abe7c06139047d7f701e
Jean-Serge <j.monbailly@orange.Fr>
2015-03-13 11:17:35 +0100

    Documentation

--- src/entite/Sommet.java
+++ src/entite/Sommet.java
@@ -9,6 +9,10 @@ public class Sommet {
 
 	private int id;
 	
+	// 	========================================================================================= 
+	//	Constructeurs
+	// 	=========================================
+	
 	/**
 	 * Instancie un nouveau sommet à partir d'un identifiant.
 	 * @param id
@@ -17,11 +21,15 @@ public class Sommet {
 		this.id = id;
 	}
 	
+	// 	========================================================================================= 
+	//	Fonctions utiles
+	// 	=========================================
+	
 	/**
 	 * Cette fonction n'est pas vraiment surchargée car elle permet
 	 * de vérifier s'il s'agit bien du bon sommet dans le bon graphe.
-	 * @param autreSommet
-	 * @return
+	 * @param autreSommet le sommet à comparer au sommet courant
+	 * @return true s'il s'agit du même sommet.
 	 */
 	public boolean equals(Sommet autreSommet){
 		return super.equals(autreSommet);
