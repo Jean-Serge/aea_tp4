075bde0c09d087e13764754692b9632637c082e5
Jean-Serge <j.monbailly@orange.Fr>
2015-03-13 11:06:00 +0100

    Redéfinition d'une Arête.

--- src/entite/Arete.java
+++ src/entite/Arete.java
@@ -2,11 +2,26 @@ package entite;
 
 /**
  * Cette classe représente une arête du graphe.
+ * Une arête est représentée par son sommet d'origine et celui
+ * d'arrivée.
  * 
  * @author Jean-Serge Monbailly
  * @author Thibaud Verbaere
  *
  */
 public class Arete {
+
+	private Sommet origine;
+	private Sommet arrivee;
+	
+	/**
+	 * 
+	 * @param origine
+	 * @param arrivee
+	 */
+	public Arete(Sommet origine, Sommet arrivee) {
+		this.origine = origine;
+		this.arrivee = arrivee;
+	}
 
 }
