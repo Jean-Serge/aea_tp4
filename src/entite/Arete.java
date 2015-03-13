05b37cfd12d899c37eeaed532f8db473381da595
Jean-Serge <j.monbailly@orange.Fr>
2015-03-13 11:12:48 +0100

    Ajout de la fonction permettant de tester l'égalité de 2 arêtes.

--- src/entite/Arete.java
+++ src/entite/Arete.java
@@ -1,19 +1,18 @@
 package entite;
 
 /**
- * Cette classe représente une arête du graphe.
- * Une arête est représentée par son sommet d'origine et celui
- * d'arrivée.
+ * Cette classe représente une arête du graphe. Une arête est représentée par
+ * son sommet d'origine et celui d'arrivée.
  * 
  * @author Jean-Serge Monbailly
  * @author Thibaud Verbaere
- *
+ * 
  */
 public class Arete {
 
 	private Sommet origine;
 	private Sommet arrivee;
-	
+
 	/**
 	 * 
 	 * @param origine
@@ -22,6 +21,19 @@ public class Arete {
 	public Arete(Sommet origine, Sommet arrivee) {
 		this.origine = origine;
 		this.arrivee = arrivee;
+	}
+
+	public boolean equals(Arete autreArete) {
+		return this.origine.equals(autreArete.getOrigine())
+				&& this.arrivee.equals(autreArete.getArrivee());
+	}
+
+	public Sommet getOrigine() {
+		return origine;
+	}
+
+	public Sommet getArrivee() {
+		return arrivee;
 	}
 
 }
