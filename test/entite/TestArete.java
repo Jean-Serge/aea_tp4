34715c844997ac38ce89b464dadb31fb9b179241
Jean-Serge <j.monbailly@orange.Fr>
2015-03-20 11:28:42 +0100

    Les tests des Aretes ont été complétés

--- test/entite/TestArete.java
+++ test/entite/TestArete.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 
 public class TestArete {
 
-	private Arete a1, a2, a3;
+	private Arete a1, a2, a3, a4, a5, a6;
 
 	@Before
 	public void setUp(){
@@ -20,6 +20,9 @@ public class TestArete {
 		a1 = new Arete(s1, s2);
 		a2 = new Arete(s1, s3);
 		a3 = new Arete(s1, s2);
+		a4 = new Arete(s2, s1);
+		a5 = new Arete(s3, s1);
+		a6 = new Arete(s2, s1);
 	}
 	
 	/**
@@ -32,5 +35,11 @@ public class TestArete {
 		assertTrue(a1.equals(a3));
 		assertTrue(a3.equals(a1));
 		assertFalse(a3.equals(a2));
-	}
+		
+		assertTrue(a1.equals(a4));
+		assertTrue(a1.equals(a6));
+		assertTrue(a2.equals(a5));
+		assertTrue(a4.equals(a1));
+		assertTrue(a6.equals(a1));
+		}
 }
