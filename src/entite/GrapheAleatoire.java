6393a9ce64b30fd979fdc205a35daf3d09bcf2b1
Jean-Serge <j.monbailly@orange.Fr>
2015-03-20 11:29:23 +0100

    Modification de la génération aléatoire pour ne pas générer plusieurs fois la même arete.

--- src/entite/GrapheAleatoire.java
+++ src/entite/GrapheAleatoire.java
@@ -3,77 +3,85 @@ package entite;
 import utils.MyRandom;
 
 /**
- * Classe permettant de générer un Graphe aélatoire pour un nombre
- * de sommets et d'arêtes fixés.
+ * Classe permettant de générer un Graphe aélatoire pour un nombre de sommets et
+ * d'arêtes fixés.
  * 
  * @author Jean-Serge Monbailly
  * @author Thibaud Verbaere
- *
+ * 
  */
-public class GrapheAleatoire extends AbstractGraphe{
-	
+public class GrapheAleatoire extends AbstractGraphe {
+
 	private int nSommets;
 	private int nAretes;
-	
-	// 	========================================================================================= 
-	//	Constructeurs
-	// 	=========================================
-	
+
+	// =========================================================================================
+	// Constructeurs
+	// =========================================
+
 	/**
-	 * Permet d'initialiser un graphe connaissant son nombre de sommets
-	 * et d'arêtes.
-	 * @param nSommets le nombre de sommets du graphe
-	 * @param nAretes le nombre d'arêtes du graphe.
+	 * Permet d'initialiser un graphe connaissant son nombre de sommets et
+	 * d'arêtes.
+	 * 
+	 * @param nSommets
+	 *            le nombre de sommets du graphe
+	 * @param nAretes
+	 *            le nombre d'arêtes du graphe.
 	 */
 	public GrapheAleatoire(int nSommets, int nAretes) {
 		super();
 		this.nSommets = nSommets;
 		this.nAretes = nAretes;
-		
+
 		this.generereGrapheAleatoire();
 	}
-	
-	// 	========================================================================================= 
-	//	Génération du graphe
-	// 	=========================================
-	
+
+	// =========================================================================================
+	// Génération du graphe
+	// =========================================
+
 	/**
-	 * Génère le graphe aléatoirement.
-	 * Commence par générer les sommets du graphe puis génère aléatoirement
-	 * le nombre d'arêtes voulu.
+	 * Génère le graphe aléatoirement. Commence par générer les sommets du
+	 * graphe puis génère aléatoirement le nombre d'arêtes voulu.
 	 */
-	private void generereGrapheAleatoire(){
+	private void generereGrapheAleatoire() {
 		// Génération des sommets
-		for(int i = 0 ; i < this.nSommets ; i++){
+		for (int i = 0; i < this.nSommets; i++) {
 			this.sommets.add(new Sommet(i));
 		}
-	
+
 		MyRandom rand = new MyRandom();
-		int cptAretesCrees	 = 0;
-		Arete areteACreer;
+		int cptAretesCrees = 0;
+		Arete areteACreer = null;
 		int debut, fin;
-		while(cptAretesCrees < this.nAretes){
+		boolean egal;
+
+		while (cptAretesCrees < this.nAretes) {
 			debut = rand.nextPositive() % this.nSommets;
 			fin = rand.nextPositive() % this.nSommets;
 			// Pas d'arête d'un sommet vers lui-même
-			if(debut == fin)
+			if (debut == fin)
 				continue;
-			
-			areteACreer = new Arete(this.sommets.get(debut), this.sommets.get(fin));
-			for(int i = 0 ; i < cptAretesCrees ; i++){
-				// On vérifie que l'arête en question n'existe pas déjà
-				if(this.aretes.get(i).equals(areteACreer)){
-					continue;
+
+			areteACreer = new Arete(this.sommets.get(debut),
+					this.sommets.get(fin));
+			// On vérifie que l'arête en question n'existe pas déjà
+			egal = false;
+			for (Arete a : this.aretes) {
+				if (areteACreer.equals(a)){
+					egal = true;
+					break;
 				}
 			}
-			
+
+			if (egal)
+				continue;
+
 			// Si tout se passe bien on ajoute cet Arête
-			
 			this.aretes.add(areteACreer);
 			cptAretesCrees++;
 		}
-		
+
 	}
-	
 
 }
